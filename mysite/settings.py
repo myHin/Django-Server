c1348d9f2a53fce2ca83463ff7e139b7432c2c7d
myHin <chanlojki2@gmail.com>
2022-12-23 20:06:34 +0800

    up

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 ALLOWED_HOSTS = ["*"]
 
